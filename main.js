05804db0c71c8deb40c638ea7d6f1ac4d18f4fb7
Jeremiah Black <jeremiahblackol@gmail.com>
2020-01-13 14:32:59 -0700

    add task cards

--- main.js
+++ main.js
@@ -1,22 +1,57 @@
+var taskArray = [];
 var addTaskToList = document.getElementById('add-task-to-list');
 var insertTextButton = document.querySelector('.insert-text-button');
 var taskTitleInput = document.querySelector('#task-title-input');
 var taskListInput = document.querySelector('#task-item-input');
+var placeTaskHere = document.querySelector('.insert-text-space');
+var taskCardSpace = document.querySelector('.white-side');
+var makeTaskListButton = document.querySelector('#make-task-card');
+// var deleteListItemButton = document.getElementById('delete-list-item');
 var blueParent = document.querySelector('.blue-side');
+var taskId = Date.now();
 
 addTaskToList.addEventListener('click', createTaskList);
+makeTaskListButton.addEventListener('click', makeTaskCard);
+// blueParent.addEventListener('click', deleteListItem);
+
 
 function createTaskList(event){
-  var placeTaskHere = document.querySelector('.insert-text-space');
   var taskListValue = taskListInput.value;
-  // placeTaskHere.innerText = taskListValue;
-  placeTaskHere.insertAdjacentHTML('beforeend', `<div id='place-text-here'>${taskListValue}</div>`);
-  // placeTaskHere.insertAdjacentElement('beforebegin',`<div>${taskListValue}</div>);
-
-
+  placeTaskHere.insertAdjacentHTML('beforeend', `<div><button id=${taskId} class="delete-list-item"type="submit" name="button">${'X'}</button>
+  ${taskListValue}</div>`);
   clearTaskInputFields();
-  }
+}
 
 function clearTaskInputFields(event){
   taskListInput.value = '';
+}
+
+// add an event listener around just the tasks
+// function deleteListItem(event){
+//   var deleteListItemButton = document.querySelectorAll('.delete-list-item');
+//   if(event.target === deleteListItemButton){
+//     console.log('hey')
+//   }
+// }
+
+function disableMakeTaskCardButton(){
+  if(taskListInput.innerText != "" && taskTitleInput.innerText != ""){
+    makeTaskListButton.setAttribute('disabled', 'disabled')
+  }
+}
+
+function makeTaskCard(event){
+  var tasks = placeTaskHere.innerText;
+  var taskCardTitle = document.querySelector('#task-title-input').value;
+  taskCardSpace.insertAdjacentHTML('beforeend', `<div id=${taskId}><div id='task-card'><h3>${taskCardTitle}</h3><div class="block-list">${tasks}</div><img class="urgent-button" src="assets/flash.svg" alt="">
+  </div></div>`)
+  taskArray.push(tasks)
+  window.localStorage.setItem('some-name', `${tasks}`);
+  clearTaskList(event);
+}
+
+
+
+function clearTaskList(event){;
+  placeTaskHere.innerText = '';
 }
